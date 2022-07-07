8f44ad343529c9106bf25304527e07bf800436dd
8thgencore <axelmaker@ya.ru>
2022-07-07 23:05:30 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/ru/skillbranch/gift_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.skillbranch.gift_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/registration/view/registration_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RegistrationPage extends StatelessWidget {
+  const RegistrationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('RegistrationPage'),),
+    );
+  }
+}
