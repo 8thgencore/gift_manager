1ac8c2466a9a3520668d5b16cac877f0c14a3228
8thgencore <axelmaker@ya.ru>
2022-07-09 18:50:59 +0300

    Login page ui

--- lib/presentation/login/view/login_page.dart
+++ lib/presentation/login/view/login_page.dart
@@ -5,8 +5,73 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('LoginPage'),),
+    return const Scaffold(
+      body: _LoginPageWidget(),
+    );
+  }
+}
+
+class _LoginPageWidget extends StatelessWidget {
+  const _LoginPageWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Expanded(flex: 4, child: SizedBox.expand()),
+        Spacer(flex: 4),
+        Center(
+          child: Text(
+            'Вход',
+            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
+          ),
+        ),
+        Spacer(flex: 5),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 36),
+          child: TextField(
+            decoration: InputDecoration(hintText: 'Почта'),
+          ),
+        ),
+        SizedBox(height: 8),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 36),
+          child: TextField(
+            decoration: InputDecoration(hintText: 'Пароль'),
+          ),
+        ),
+        SizedBox(height: 40),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 36),
+          child: SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () => debugPrint('Нажали на кнопку Войти'),
+              child: Text('Войти'),
+            ),
+          ),
+        ),
+        SizedBox(height: 24),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Еще нет аккаунта?"),
+            TextButton(
+              onPressed: () => debugPrint('Нажали на кнопку Создать'),
+              child: Text('Создать'),
+            ),
+          ],
+        ),
+        SizedBox(height: 8),
+        TextButton(
+          onPressed: () => debugPrint('Нажали на кнопку Не помню пароль'),
+          child: Text('Не помню пароль'),
+        ),
+        Spacer(flex: 16),
+// 19:25
+      ],
     );
   }
 }
