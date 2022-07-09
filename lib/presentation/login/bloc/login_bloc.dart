d25154f74fc04f1806565cc67c3a0b926ac1a68f
8thgencore <axelmaker@ya.ru>
2022-07-09 23:26:49 +0300

    Added bloc widgets on login page

--- lib/presentation/login/bloc/login_bloc.dart
+++ lib/presentation/login/bloc/login_bloc.dart
@@ -10,8 +10,25 @@ part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc() : super(LoginState.initial()) {
-    on<LoginLoginButtonClicked>((event, emit) {
-      emit(state.copyWith(authenticated: true));
-    });
+    on<LoginLoginButtonClicked>(_loginButtonClicked);
+  }
+
+  FutureOr<void> _loginButtonClicked(
+    LoginLoginButtonClicked event,
+    Emitter<LoginState> emit,
+  ) {
+    emit(state.copyWith(authenticated: true));
+  }
+
+  @override
+  void onEvent(LoginEvent event) {
+    debugPrint('Login Bloc. Event happened: $event');
+    super.onEvent(event);
+  }
+
+  @override
+  void onTransition(Transition<LoginEvent, LoginState> transition) {
+    debugPrint('Login Bloc. Transition happened: $transition');
+    super.onTransition(transition);
   }
 }
--- lib/presentation/login/view/login_page.dart
+++ lib/presentation/login/view/login_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gift_manager/presentation/home/view/home_page.dart';
+import 'package:gift_manager/presentation/login/bloc/login_bloc.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _LoginPageWidget(),
+    return BlocProvider(
+      create: (context) => LoginBloc(),
+      child: const Scaffold(
+        body: _LoginPageWidget(),
+      ),
     );
   }
 }
@@ -18,60 +24,71 @@ class _LoginPageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Expanded(flex: 4, child: SizedBox.expand()),
-        Spacer(flex: 4),
-        Center(
-          child: Text(
-            'Вход',
-            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
-          ),
-        ),
-        Spacer(flex: 5),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 36),
-          child: TextField(
-            decoration: InputDecoration(hintText: 'Почта'),
-          ),
-        ),
-        SizedBox(height: 8),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 36),
-          child: TextField(
-            decoration: InputDecoration(hintText: 'Пароль'),
-          ),
-        ),
-        SizedBox(height: 40),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 36),
-          child: SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () => debugPrint('Нажали на кнопку Войти'),
-              child: Text('Войти'),
+    return BlocListener<LoginBloc, LoginState>(
+      listener: (context, state) {
+        if (state.authenticated) {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (_) => const HomePage()),
+          );
+        }
+      },
+      child: Column(
+        children: [
+          // Expanded(flex: 4, child: SizedBox.expand()),
+          const Spacer(flex: 4),
+          const Center(
+            child: Text(
+              'Вход',
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
             ),
           ),
-        ),
-        SizedBox(height: 24),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text("Еще нет аккаунта?"),
-            TextButton(
-              onPressed: () => debugPrint('Нажали на кнопку Создать'),
-              child: Text('Создать'),
+          const Spacer(flex: 5),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 36),
+            child: TextField(
+              decoration: InputDecoration(hintText: 'Почта'),
             ),
-          ],
-        ),
-        SizedBox(height: 8),
-        TextButton(
-          onPressed: () => debugPrint('Нажали на кнопку Не помню пароль'),
-          child: Text('Не помню пароль'),
-        ),
-        Spacer(flex: 16),
+          ),
+          const SizedBox(height: 8),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 36),
+            child: TextField(
+              decoration: InputDecoration(hintText: 'Пароль'),
+            ),
+          ),
+          const SizedBox(height: 40),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 36),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () => context
+                    .read<LoginBloc>()
+                    .add(const LoginLoginButtonClicked()),
+                child: const Text('Войти'),
+              ),
+            ),
+          ),
+          const SizedBox(height: 24),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text("Еще нет аккаунта?"),
+              TextButton(
+                onPressed: () => debugPrint('Нажали на кнопку Создать'),
+                child: const Text('Создать'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8),
+          TextButton(
+            onPressed: () => debugPrint('Нажали на кнопку Не помню пароль'),
+            child: const Text('Не помню пароль'),
+          ),
+          const Spacer(flex: 16),
 // 19:25
-      ],
+        ],
+      ),
     );
   }
 }
