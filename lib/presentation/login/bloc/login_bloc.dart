23e90ab3aab68649f3e84d4ce422bfd9dcc7da8f
8thgencore <axelmaker@ya.ru>
2022-07-09 22:46:40 +0300

    Login bloc state

--- lib/presentation/login/bloc/login_bloc.dart
+++ lib/presentation/login/bloc/login_bloc.dart
@@ -2,14 +2,16 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 part 'login_event.dart';
+
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial()) {
-    on<LoginEvent>((event, emit) {
-      // TODO: implement event handler
+  LoginBloc() : super(LoginState.initial()) {
+    on<LoginLoginButtonClicked>((event, emit) {
+      emit(state.copyWith(authenticated: true));
     });
   }
 }
--- lib/presentation/login/bloc/login_state.dart
+++ lib/presentation/login/bloc/login_state.dart
@@ -1,10 +1,45 @@
 part of 'login_bloc.dart';
 
-abstract class LoginState extends Equatable {
-  const LoginState();
-}
+class LoginState extends Equatable {
+  final String email;
+  final String password;
+  final bool emailValid;
+  final bool passwordValid;
+  final bool authenticated;
 
-class LoginInitial extends LoginState {
+  const LoginState({
+    required this.email,
+    required this.password,
+    required this.emailValid,
+    required this.passwordValid,
+    required this.authenticated,
+  });
+
+  factory LoginState.initial() => const LoginState(
+        email: '',
+        password: '',
+        emailValid: false,
+        passwordValid: false,
+        authenticated: false,
+      );
+
+  LoginState copyWith({
+    final String? email,
+    final String? password,
+    final bool? emailValid,
+    final bool? passwordValid,
+    final bool? authenticated,
+  }) {
+    return LoginState(
+      email: email ?? this.email,
+      password: password ?? this.password,
+      emailValid: emailValid ?? this.emailValid,
+      passwordValid: passwordValid ?? this.passwordValid,
+      authenticated: authenticated ?? this.authenticated,
+    );
+  }
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props =>
+      [email, password, emailValid, passwordValid, authenticated];
 }
