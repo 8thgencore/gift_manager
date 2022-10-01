94b5ef2a69d8b47eac54793bd907e745cefbb2cc
8thgencore <axelmaker@ya.ru>
2022-10-01 19:38:46 +0300

    Change avatar functionality

--- lib/presentation/registration/bloc/registration_bloc.dart
+++ lib/presentation/registration/bloc/registration_bloc.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:gift_manager/data/modal/request_error.dart';
@@ -8,9 +11,23 @@ part 'registration_event.dart';
 part 'registration_state.dart';
 
 class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
-  RegistrationBloc() : super(RegistrationFieldsInfo(avatarLink: '')) {
-    on<RegistrationEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  RegistrationBloc()
+      : super(RegistrationFieldsInfo(
+          avatarLink: _avatarBuilder(_defaultAvatarKey),
+        )) {
+    on<RegistrationChangeAvatar>(_onChangeAvatar);
+  }
+
+  static const _defaultAvatarKey = 'test';
+  String _avatarKey = _defaultAvatarKey;
+
+  static String _avatarBuilder(String key) => 'https://avatars.dicebear.com/api/croodles/$key.svg';
+
+  FutureOr<void> _onChangeAvatar(
+    final RegistrationChangeAvatar event,
+    final Emitter<RegistrationState> emit,
+  ) {
+    _avatarKey = Random().nextInt(1000000).toString();
+    emit(RegistrationFieldsInfo(avatarLink: _avatarBuilder(_avatarKey)));
   }
 }
--- lib/presentation/registration/view/registration_page.dart
+++ lib/presentation/registration/view/registration_page.dart
@@ -56,10 +56,16 @@ class _AvatarWidget extends StatelessWidget {
       ),
       child: Row(
         children: [
-          SvgPicture.network(
-            'https://avatars.dicebear.com/api/croodles/test.svg',
-            height: 48,
-            width: 48,
+          BlocBuilder<RegistrationBloc, RegistrationState>(
+            buildWhen: (_, current) => current is RegistrationFieldsInfo,
+            builder: (context, state) {
+              final fieldsInfo = state as RegistrationFieldsInfo;
+              return SvgPicture.network(
+                fieldsInfo.avatarLink,
+                height: 48,
+                width: 48,
+              );
+            },
           ),
           const SizedBox(width: 8),
           Text('Ваш аватар', style: context.theme.h3),
