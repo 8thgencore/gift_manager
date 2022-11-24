bdfa41c175c5f0e57713d4bcbbd075129ea733b2
8thgencore <axelmaker@ya.ru>
2022-11-24 23:09:37 +0300

    Gifts widget

--- lib/presentation/gifts/view/gifts_page.dart
+++ lib/presentation/gifts/view/gifts_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gift_manager/data/http/model/gift_dto.dart';
 import 'package:gift_manager/di/service_locator.dart';
 import 'package:gift_manager/presentation/gifts/bloc/gifts_bloc.dart';
+import 'package:gift_manager/resources/app_colors.dart';
 import 'package:gift_manager/resources/illustrations.dart';
 
 class GiftsPage extends StatelessWidget {
@@ -18,7 +20,7 @@ class GiftsPage extends StatelessWidget {
 }
 
 class _GiftsPageWidget extends StatelessWidget {
-  const _GiftsPageWidget({super.key});
+  const _GiftsPageWidget();
 
   @override
   Widget build(BuildContext context) {
@@ -33,12 +35,14 @@ class _GiftsPageWidget extends StatelessWidget {
           } else if (state is InitialLoadingErrorState) {
             return const _InitialLoadingErrorWidget();
           } else if (state is LoadedGiftsState) {
-            //TODO show gifts
+            return _GiftsListWidget(
+              gifts: state.gifts,
+              showLoading: state.showLoading,
+              showError: state.showError,
+            );
           }
           debugPrint('Unknown state: $state');
-          //TODO loading error state
-
-          return const Text('HomePage');
+          return const Center(child: Text('GIFTS PAGE'));
         },
       ),
     );
@@ -46,7 +50,7 @@ class _GiftsPageWidget extends StatelessWidget {
 }
 
 class _LoadingWidget extends StatelessWidget {
-  const _LoadingWidget({super.key});
+  const _LoadingWidget();
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +59,7 @@ class _LoadingWidget extends StatelessWidget {
 }
 
 class _NoGiftsWidget extends StatelessWidget {
-  const _NoGiftsWidget({super.key});
+  const _NoGiftsWidget();
 
   @override
   Widget build(BuildContext context) {
@@ -80,7 +84,7 @@ class _NoGiftsWidget extends StatelessWidget {
 }
 
 class _InitialLoadingErrorWidget extends StatelessWidget {
-  const _InitialLoadingErrorWidget({super.key});
+  const _InitialLoadingErrorWidget();
 
   @override
   Widget build(BuildContext context) {
@@ -112,4 +116,82 @@ class _InitialLoadingErrorWidget extends StatelessWidget {
       ],
     );
   }
+}
+
+class _GiftsListWidget extends StatelessWidget {
+  const _GiftsListWidget({
+    required this.gifts,
+    required this.showLoading,
+    required this.showError,
+  });
+
+  final List<GiftDto> gifts;
+  final bool showLoading;
+  final bool showError;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+      separatorBuilder: (_, __) => const SizedBox(height: 12),
+      itemCount: gifts.length + 1 + (_haveExtraWidget ? 1 : 0),
+      itemBuilder: (context, index) {
+        if (index == 0) {
+          return const Text(
+            'Подарки:',
+            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
+          );
+        }
+        if (index == gifts.length + 1) {
+          if (showLoading) {
+            return Container(
+              height: 56,
+              alignment: Alignment.center,
+              child: const CircularProgressIndicator(),
+            );
+          } else {
+            if (!showError) {
+              debugPrint('index = gifts.length + 1 but showLoading = false && showError = false');
+            }
+            return Container(
+              height: 56,
+              alignment: Alignment.center,
+              child: const Text('ERROR'),
+            );
+          }
+        }
+        final gift = gifts[index - 1];
+        return Container(
+          height: 88,
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            // TODO
+            color: const Color(0xFFFF0F2F7),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                gift.name,
+                style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
+              ),
+              const SizedBox(height: 6),
+              const Text(
+                'Подпись подарка',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                  color: AppColors.lightGrey100,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  bool get _haveExtraWidget => showLoading || showError;
 }
