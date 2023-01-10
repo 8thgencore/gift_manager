7b86feb02ca07c431ce9af22819ec3cffa417657
8thgencore <axelmaker@ya.ru>
2023-01-10 23:26:37 +0300

    added bottom navbar

--- lib/presentation/home/view/home_page.dart
+++ lib/presentation/home/view/home_page.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gift_manager/di/service_locator.dart';
 import 'package:gift_manager/navigation/route_name.dart';
+import 'package:gift_manager/presentation/gifts/view/gifts_page.dart';
 import 'package:gift_manager/presentation/home/bloc/home_bloc.dart';
+import 'package:gift_manager/presentation/home/models/bottom_tab.dart';
+import 'package:gift_manager/presentation/people/view/people_page.dart';
+import 'package:gift_manager/presentation/settings/view/settings_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -16,8 +20,27 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class _HomePageWidget extends StatelessWidget {
+class _HomePageWidget extends StatefulWidget {
   const _HomePageWidget({super.key});
+
+  @override
+  State<_HomePageWidget> createState() => _HomePageWidgetState();
+}
+
+class _HomePageWidgetState extends State<_HomePageWidget> {
+  BottomTab _currentTab = BottomTab.gifts;
+
+  final _pages = const [
+    GiftsPage(),
+    PeoplePage(),
+    SettingsPage(),
+  ];
+
+  void _changeTab(int index) {
+    setState(() {
+      _currentTab = BottomTab.values[index];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,33 +55,27 @@ class _HomePageWidget extends StatelessWidget {
       },
       child: Scaffold(
         appBar: AppBar(),
-        body: Center(
-          child: Column(
-            children: [
-              BlocBuilder<HomeBloc, HomeState>(
-                builder: (context, state) {
-                  if (state is HomeWithUser) {
-                    return Text("${state.user.toString()}\n\n${state.gifts.join('\n')}");
-                  }
-                  return const Text('HomePage');
-                },
-              ),
-              const SizedBox(height: 42),
-              TextButton(
-                onPressed: () async {
-                  context.read<HomeBloc>().add(const HomeLogoutPushed());
-                },
-                child: const Text('Logout'),
-              ),
-              const SizedBox(height: 42),
-              ElevatedButton(
-                onPressed: () => Navigator.of(context).pushNamed(RouteName.gifts.route),
-                child: const Text('Open presents'),
-              )
-            ],
-          ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentTab.index,
+          onTap: _changeTab,
+          items: [
+            _createBottomNavBarItem(BottomTab.gifts),
+            _createBottomNavBarItem(BottomTab.people),
+            _createBottomNavBarItem(BottomTab.settings),
+          ],
+        ),
+        body: IndexedStack(
+          index: _currentTab.index,
+          children: _pages,
         ),
       ),
+    );
+  }
+
+  BottomNavigationBarItem _createBottomNavBarItem(BottomTab tab) {
+    return BottomNavigationBarItem(
+      icon: Icon(tab.icon),
+      label: tab.label,
     );
   }
 }
